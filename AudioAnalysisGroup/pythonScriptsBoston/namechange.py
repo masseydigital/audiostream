66400d31b5edd002d2e7ca75cf9a86ea08e61a2c
Zachmann21 <b.a.z.21@hotmail.com>
2015-10-08 10:01:32 -0700

    namechange method updated

--- AudioAnalysisGroup/pythonScriptsBoston/namechange.py
+++ AudioAnalysisGroup/pythonScriptsBoston/namechange.py
@@ -2,7 +2,7 @@
 
 ti = "Hello, Brooklyn"
 ar = "All Time Low"
-al = "Nothing Personal"
+al = "Nothing Personal this is a really long title that is more than 20 characters"
 
 
 
@@ -14,9 +14,22 @@ def titleformat(title, artist, album):
 	albumlow = album.lower()
 	albumfinal = albumlow.replace(" ", "_")
 
-	return titlefinal + "-" + artistfinal + "-" + albumfinal + ".mp3"
+	#final = titlefinal + "-" + artistfinal + "-" + albumfinal
+	finalstrings = [titlefinal, artistfinal, albumfinal]
+	outstrings = ["", "", ""]
+	acceptedc = ['_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+	
+	for x in range(0, 3):
+		for character in finalstrings[x]:
+			if character in	acceptedc:
+				if len(outstrings[x]) < 30:
+					outstrings[x]= outstrings[x] + character
 
+	out = outstrings[0] + "-" + outstrings[1] + "-" + outstrings[2] + ".mp3"
+	print outstrings
+	return out
 
+	
 
 newtitle = titleformat(title = ti, artist = ar, album = al)
 print newtitle
