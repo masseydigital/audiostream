9a09d83fe7f9fb21a3fda941a73d1f57c7b1af77
Zachmann21 <b.a.z.21@hotmail.com>
2015-10-09 14:28:07 -0700

    namechange method now changes filename

--- AudioAnalysisGroup/pythonScriptsBoston/namechange.py
+++ AudioAnalysisGroup/pythonScriptsBoston/namechange.py
@@ -1,23 +1,25 @@
+import os
 
-
+fil = "/home/bzachmann/Documents/test.mp3"
 ti = "Hello, Brooklyn"
 ar = "All Time Low"
-al = "Nothing Personal this is a really long title that is more than 20 characters"
+al = "Nothing Personal"
 
 
-
-def titleformat(title, artist, album):
+#takes the full filepath (example: /home/bzachmann/Documents/test.mp3) 
+#and some meta data to format a new filename and rename the file in the filepath
+#the function returns the new filepath
+def changename(filepath, title, artist, album):
 	titlelow = title.lower()
-	titlefinal = titlelow.replace(" ", "_")
+	titlefinal = titlelow.replace(" ", "-")
 	artistlow = artist.lower()
-	artistfinal = artistlow.replace(" ", "_")
+	artistfinal = artistlow.replace(" ", "-")
 	albumlow = album.lower()
-	albumfinal = albumlow.replace(" ", "_")
+	albumfinal = albumlow.replace(" ", "-")
 
-	#final = titlefinal + "-" + artistfinal + "-" + albumfinal
 	finalstrings = [titlefinal, artistfinal, albumfinal]
 	outstrings = ["", "", ""]
-	acceptedc = ['_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+	acceptedc = ['-','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
 	
 	for x in range(0, 3):
 		for character in finalstrings[x]:
@@ -25,12 +27,16 @@ def titleformat(title, artist, album):
 				if len(outstrings[x]) < 30:
 					outstrings[x]= outstrings[x] + character
 
-	out = outstrings[0] + "-" + outstrings[1] + "-" + outstrings[2] + ".mp3"
-	print outstrings
-	return out
+	newname = outstrings[0] + "_" + outstrings[1] + "_" + outstrings[2] + ".mp3"
 
+	filesplits = os.path.split(filepath)
+	path = filesplits[0]
+	
+	newfilepath = os.path.join(path, newname)
+	os.rename(filepath, newfilepath)
+	return newfilepath
+	
 	
 
-newtitle = titleformat(title = ti, artist = ar, album = al)
-print newtitle
-
+newpath = changename(fil, ti, ar, al)
+print newpath
