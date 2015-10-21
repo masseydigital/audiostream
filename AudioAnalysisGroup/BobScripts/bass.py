36adec9f1952d3086d46bf6557065c2afceb19f7
Bob <Robert.Opgrand@ndsu.edu>
2015-10-21 17:34:48 -0500

    updated bass

--- AudioAnalysisGroup/BobScripts/bass.py
+++ AudioAnalysisGroup/BobScripts/bass.py
@@ -1,3 +1,5 @@
+# this function computes the energy from between frequency 0 to 150hz
+
 import essentia
 import essentia.standard
 import sys
@@ -14,7 +16,12 @@ def bassCalculator(audioInput):
 	bass = bassExtractor(audio)
 	return bass
 
-total = bassCalculator(audioInput = audio)[0] + bassCalculator(audioInput = audio)[1] + bassCalculator(audioInput = audio)[2]
+bass = bassCalculator(audioInput = audio)
+total = 0.0
+for i in range(0,3):
+	total += bass[i]
+#for i in range(0,23):
+#	print 'Band: %d %d' %(i, bass[i])
+print "%d" %total
 
-print 'Bass: %f' % total
 
