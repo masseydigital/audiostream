47047f47b35cc485e0374352fd7291652245b01c
Joseph Mitchell <jmitch930@gmail.com>
2015-10-21 19:17:04 -0500

    Formatted Buttons, moved style from index.html to sidebarStyle.css

--- WebAndAppGroup/Website/public/javascript/toggleMenuScript.js
+++ WebAndAppGroup/Website/public/javascript/toggleMenuScript.js
@@ -1,29 +1,35 @@
 $(window).load(function(){
-$(document).ready(function() {
-  sidebarStatus = false;
-  $('.sidebar-toggle').click(function() {
-    if (sidebarStatus == false) {
-      $('.sidebar').animate({
-        marginLeft: "0px",
-        opacity: "1"
-      }, 1000);
-      $('.content').animate({
-        marginLeft: "150px",
-        opacity: "1"
-      }, 1000);
-      sidebarStatus = true;
-    }
-    else {
-      $('.sidebar').animate({
-        marginLeft: "-150px",
-        opacity: "1"
-      }, 1000);
-      $('.content').animate({
-        marginLeft: "0px",
-        opacity: "1"
-      }, 1000);
-      sidebarStatus = false;
-    }
-  });
-});
+	$(document).ready(function() {
+		sidebarStatus = false;
+		$('.sidebar-toggle').click(function() {
+			if (sidebarStatus == false) {
+				
+				$('.sidebar').animate({
+					marginLeft: "0px",
+					opacity: "1"
+				}, 1000);
+			
+				$('.content').animate({
+					marginLeft: "150px",
+					opacity: "1"
+				}, 1000);
+				
+				sidebarStatus = true;
+			}
+			
+			else {
+				$('.sidebar').animate({
+					marginLeft: "-150px",
+					opacity: "1"
+				}, 1000);
+			  
+				$('.content').animate({
+					marginLeft: "0px",
+					opacity: "1"
+					}, 1000);
+					
+				sidebarStatus = false;
+			}	
+		});
+	});
 });
