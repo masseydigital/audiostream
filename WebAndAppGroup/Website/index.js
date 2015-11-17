40d564228f8dbf18a5240241c029d2318e1366ab
Joseph Mitchell <jmitch930@gmail.com>
2015-11-17 12:44:41 -0600

    Database support
    Added a basic database connection
    If you get a connection error you probably don't have a local database

--- WebAndAppGroup/Website/index.js
+++ WebAndAppGroup/Website/index.js
@@ -2,16 +2,49 @@
 var express = require('express');
 var colors = require('colors');
 var jquery = require('jquery');
+var mysql = require('mysql');
 
 var app = express();
 var port = 8000;
 
+var pool = mysql.createPool({
+    host     : 'localhost',
+    user     : 'root',
+    password : '1111',
+    database : 'test',
+    debug    :  false
+});
+
+function handle_database(req, res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          //connection.release();
+          //res.json({"code" : 100, "status" : "Error in connection database"});
+		  console.log("Error connecting to the database");
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+		
+		connection.query("select * from test",function(err,rows){
+            //connection.release();
+            if(!err) {
+                //res.json(rows);
+				console.log(rows[0]);
+            }           
+        });
+  });
+}
+
+console.log("Test text");
 //Where html documents grab stuff for the site
 app.use(express.static('public'));
 
 //Defining url directories
 //Default Directory
 app.get('/', function(req, res) {
+	handle_database(req, res);
     res.sendFile(__dirname + '/views/index.html');
 });
 //Login Directory
